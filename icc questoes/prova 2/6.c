f045816f5308112ed0b293774d42037746ec05ba
Eduardo Ribeiro Rodrigues <eduardorr.ws@gmail.com>
2022-06-23 23:14:29 -0300

    att

--- icc questoes/prova 2/6.c
+++ icc questoes/prova 2/6.c
@@ -3,3 +3,89 @@ Nome: Eduardo Ribeiro Rodrigues
 NUSP: 13696679
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
+
+void ordenar_vetor(int *vet, int tvet){
+    int aux;
+    
+    for(int n=0; n<tvet; n++){
+        for(int o=n+1; o<tvet; o++){
+            if(vet[n] > vet[o]){
+                aux = vet[n];
+                vet[n] = vet[o];
+                vet[o] = aux;
+            }
+        }
+    }
+    
+
+    
+}
+
+int main(){
+	int *A, *B, *C;
+    
+	A = (int *)malloc(1000 * sizeof(int));
+    B = (int *)malloc(1000 * sizeof(int));
+    C = (int *)malloc(2000 * sizeof(int));
+	
+   
+    
+    srand(time(NULL));
+        
+    //definindo o vetor A
+    for(int j=0; j<1000; j++){
+        A[j] = rand() % 1000;
+    }
+    ordenar_vetor(A, 1000);
+
+    
+    
+
+    //definindo o vetor B
+    for(int h=0; h<1000; h++){
+        B[h] = rand() % 1000;
+    }
+    
+    ordenar_vetor(B, 1000); 
+    
+    gerar_c(A, B, C);
+    
+
+
+    //imprimindo o vetor C
+    for(int p=0; p<2000; p++){
+        printf("%d ", C[p]);
+    }
+        
+    return 0;
+}
+
+
+void gerar_c(int *A, int *B, int *C){
+    //definindo o vetor C  
+    for(int m=0; m<2000; m++){
+        if(m<1000){
+            C[m] = A[m];
+        }
+        else{
+            C[m] = B[m - 1000];
+        }
+    }
+
+    ordenar_vetor(C, 2000);
+
+}
+
+
+
+        
+
+    
+
+
+        
+	
--- icc questoes/prova 2/7.c
+++ icc questoes/prova 2/7.c
@@ -3,3 +3,73 @@ Nome: Eduardo Ribeiro Rodrigues
 NUSP: 13696679
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int **ler_matriz(int linhas, int colunas){
+	int **mat;
+	mat = (int **)malloc(linhas * sizeof(int *));
+	for(int i = 0; i<linhas; i++){
+		mat[i] = (int *)malloc(colunas * sizeof(int));
+	}
+	for(int i = 0; i<linhas; i++){
+		for(int j=0; j<colunas; j++){
+			scanf("%d", &mat[i][j]);
+		}
+	}
+	printf("\n");
+    
+	return mat;
+}
+
+
+void subtrair_matrizes(int **mat1, int **mat2, int **matresult, int linhas, int colunas){
+
+    for(int i=0; i<linhas; i++){
+        for(int j=0; j<colunas; j++){
+            matresult[i][j] = mat1[i][j] - mat2[i][j];
+        }
+    }
+
+}
+
+void imprimir_matriz(int **mat, int linhas, int colunas){
+    for(int i=0; i<linhas; i++){
+        for(int j=0; j<colunas; j++){
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void liberar_matriz(int **mat, int linhas){
+    for(int i=0; i<linhas; i++){
+        free(mat[i]);
+    }
+    free(mat);
+}
+
+int main(void)
+{
+	int linhas, colunas, **mat1, **mat2, **matresult;
+
+	scanf("%d %d", &linhas, &colunas);
+
+    matresult = (int **)malloc(linhas * sizeof(int *));
+    for(int i=0; i<linhas; i++){
+        matresult[i] = (int *)malloc(colunas * sizeof(int));
+    }
+
+    printf("\n");
+	mat1 = ler_matriz(linhas, colunas);
+    mat2 = ler_matriz(linhas, colunas);
+
+    subtrair_matrizes(mat1, mat2, matresult, linhas, colunas);
+
+    imprimir_matriz(matresult, linhas, colunas);
+	
+    liberar_matriz(mat1, linhas);
+    liberar_matriz(mat2, linhas);
+    liberar_matriz(matresult, linhas);
+    
+}
